123992d9249def4e4b620ae31ed950bcbb0a1d71
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-21 14:42:11 -0300

    Debug resources to find answers

--- src/components/listings/Single.test.js
+++ src/components/listings/Single.test.js
@@ -4,11 +4,20 @@ import renderer from 'react-test-renderer';
 import Single from './Single';
 
 
-const fakeItem = jest.fn();
+const item = {
+  "id": 1,
+  "title": "Building an App with ReactJS and MeteorJS",
+  "category": "react",
+  "description": "Meteor and React are a powerhouse combination. Meteor gives you a fast, easy-to-use solution for data management across clients and servers, and React gives you a way to structure your app's UI from reusable components. The combination allows you to create your dream apps: dynamic, data driven, and cross-platform.",
+  "link": "https://www.lynda.com/React-js-tutorials/Building-App-React-js-MeteorJS/533228-2.html",
+  "release_date": "12/21/2016",
+  "views": "31,266",
+  "image": "https://cdn.lynda.com/course/533228/533228-636179098122239600-16x9.jpg"
+};
 
 describe('Single snapshots', () => {
   it('Single snapshot test', () => {
-    const component = renderer.create(<Single item={fakeItem} />);
+    const component = renderer.create(<Single item={item} />);
     const tree = component.toJSON();
     
     expect(tree).toMatchSnapshot();
