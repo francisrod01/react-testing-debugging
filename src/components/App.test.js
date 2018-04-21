d63dc5bf676c4c4b74ce20fcc69626a454c09094
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-21 13:15:13 -0300

    Snapshot testing with Jest

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+
 import App from './App';
 
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it('App snapshot test', () => {
+  const component = renderer.create(<App />);
+  const tree = component.toJSON();
+  
+  expect(tree).toMatchSnapshot();
 });
+
