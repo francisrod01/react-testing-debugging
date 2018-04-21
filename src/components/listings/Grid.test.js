9f7df66eb876a5bc895b3d6481fe5b8cc3aa4406
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-21 12:29:47 -0300

    Test string with matchers

--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -1,5 +1,6 @@
 import data from '../../data/courses.json';
 
+
 const numItems = data.length;
 
 test('Number of items = 12', () => {
@@ -8,4 +9,15 @@ test('Number of items = 12', () => {
 
 test('Number of items to be greater than 12', () => {
   expect(numItems).toBeGreaterThanOrEqual(12);
+});
+
+
+const dataTest = data[0].title;
+
+test('There is a JS in this title', () => {
+  expect(dataTest).toMatch(/JS/);
+});
+
+test('The title contains React', () => {
+  expect(dataTest).toContain('React');
 });
