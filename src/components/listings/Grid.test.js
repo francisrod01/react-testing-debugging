3cdf4dc4f39a3e9629007f9a7173f4fb666569d8
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-21 12:39:38 -0300

    Test arrays and objects with matchers

--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -1,6 +1,7 @@
 import data from '../../data/courses.json';
 
 
+// Numbers
 const numItems = data.length;
 
 test('Number of items = 12', () => {
@@ -12,6 +13,7 @@ test('Number of items to be greater than 12', () => {
 });
 
 
+// Strings
 const dataTest = data[0].title;
 
 test('There is a JS in this title', () => {
@@ -20,4 +22,22 @@ test('There is a JS in this title', () => {
 
 test('The title contains React', () => {
   expect(dataTest).toContain('React');
+});
+
+
+// Arrays
+const data2 = ['React Native', 'MeteorJS'];
+
+test('The list of course mentions React Native and MeteorJS', () => {
+  expect(['React Native', 'MeteorJS', 'React']).toEqual(expect.arrayContaining(data2));
+});
+
+
+// Objects
+test('The first course to have a property of Title', () => {
+  expect(data[0]).toHaveProperty('title');
+});
+
+test('The first course to have a property of Title', () => {
+  expect(data[0]).toHaveProperty('views', '31,266');
 });
